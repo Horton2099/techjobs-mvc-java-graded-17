754179d4c7b5ff730ddef9dba6112a1a74b833f4
Austin <kenjigw@gmail.com>
2023-10-21 20:46:09 -0500

    almost super bonus

---
+++ src/main/java/org/launchcode/techjobsmvc/controllers/TechJobsController.java
@@ -0,0 +1,20 @@
+package org.launchcode.techjobsmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class TechJobsController {
+
+    static HashMap<String, String> actionChoices = new HashMap<>();
+
+    static HashMap<String, String> columnChoices = new HashMap<>();
+
+    @ModelAttribute("actions")
+    public static Map<String, String> getActionChoices() {
+        return actionChoices;
+    }
+}
